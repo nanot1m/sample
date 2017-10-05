f23dc70c7092ea41af537d57537e416b603e95f1
aa <aa@skbkontur.ru>
2017-10-05 13:32:53 +0500

    Add smth

---
+++ bluewater/Configuration/ServiceOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace bluewater.Configuration
+{
+    public static class ServiceOptions
+    {
+        public static string Name { get; } = "bluewater";
+    }
+}
---
+++ bluewater/Controllers/FiltersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bluewater.Controllers
+{
+    [Route("Filters")]
+    public class FiltersController : Controller
+    { 
+    }
+}

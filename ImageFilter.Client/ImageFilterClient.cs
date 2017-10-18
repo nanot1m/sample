328b990f56fe15088ee9a2d33401e32aca8e3a5b
Alexey Kirpichnikov <alexkir@skbkontur.ru>
2017-10-18 15:32:27 +0500

    Refactor

--- ImageFilter.Client/ImageFilterClient.cs
+++ ImageFilter.Client/ImageFilterClient.cs
@@ -25,60 +25,60 @@ namespace Vostok.Sample.ImageFilter.Client
         
         public async Task<string> ApplyBlackWhiteAsync(string id)
         {
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/BlackWhite"));
+            return await MakeRequest($"ImageFilter/{id}/BlackWhite");
         }
         
         public async Task<string> ApplyContrastAsync(string id, int amount)
         {
             if (amount < -100 || amount > 100)
                 throw new ArgumentOutOfRangeException("Contrast amount must be between -100 and 100.");
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Contrast/{amount}"));
+            return await MakeRequest($"ImageFilter/{id}/Contrast/{amount}");
         }
         
         public async Task<string> ApplyInvertAsync(string id)
         {
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Invert"));
+            return await MakeRequest($"ImageFilter/{id}/Invert");
         }
         
         public async Task<string> ApplyBrightnessAsync(string id, int amount)
         {
             if (amount < -100 || amount > 100)
                 throw new ArgumentOutOfRangeException("Brightness amount must be between -100 and 100.");
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Brightness/{amount}"));
+            return await MakeRequest($"ImageFilter/{id}/Brightness/{amount}");
         }
         
         public async Task<string> ApplyPixelateAsync(string id, int size)
         {
             if (size <= 0)
                 throw new ArgumentOutOfRangeException("Pixel size must be greater than zero.");
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Pixelate/{size}"));
+            return await MakeRequest($"ImageFilter/{id}/Pixelate/{size}");
         }
         
         public async Task<string> ApplyOilPaintAsync(string id)
         {
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/OilPaint"));
+            return await MakeRequest($"ImageFilter/{id}/OilPaint");
         }
         
         public async Task<string> ApplyVignetteAsync(string id)
         {
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Vignette"));
+            return await MakeRequest($"ImageFilter/{id}/Vignette");
         }
         
         public async Task<string> ApplyGlowAsync(string id)
         {
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Glow"));
+            return await MakeRequest($"ImageFilter/{id}/Glow");
         }
         
         public async Task<string> ApplyResizeAsync(string id, int width, int height)
         {
             if (width < 0 || height < 0 || width + height == 0)
                 throw new ArgumentOutOfRangeException("Width and height must be greater than zero. One of them can be zero (to preserve aspect ratio), but not both.");
-            return await MakeRequest(Request.Post($"ImageFilter/{id}/Resize/{width}x{height}"));
+            return await MakeRequest($"ImageFilter/{id}/Resize/{width}x{height}");
         }
 
-        private async Task<string> MakeRequest(Request request)
+        private async Task<string> MakeRequest(string uri)
         {
-            var result = await cluster.SendAsync(request).ConfigureAwait(false);
+            var result = await cluster.SendAsync(Request.Post(uri)).ConfigureAwait(false);
             if (result.Response.Code == ResponseCode.NotFound)
                 return null;
             result.Response.EnsureSuccessStatusCode();
